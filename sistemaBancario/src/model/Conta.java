e751b9ff9f865021b134e0a54d4d079f77056d25
raffael-marinho <silvamarinho10@gmail.com>
2025-02-16 12:15:44 -0300

    fix: refactor Conta

--- sistemaBancario/src/model/Conta.java
+++ sistemaBancario/src/model/Conta.java
@@ -4,9 +4,8 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-public abstract class Conta {
+public abstract class Conta implements IConta {
 
 	private String numero;
 	private BigDecimal saldo;
@@ -14,7 +13,7 @@ public abstract class Conta {
 	private boolean status;
 	private List<Transacao> transacoes;
 
-	public Conta(String numero, BigDecimal saldo, LocalDateTime dataAbertura, boolean status) {
+	public Conta(String numero) {
 		this.numero = numero;
 		this.saldo = BigDecimal.ZERO.setScale(2);
 		this.dataAbertura = LocalDateTime.now();
@@ -26,24 +25,16 @@ public abstract class Conta {
 		return numero;
 	}
 
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-
 	public BigDecimal getSaldo() {
 		return saldo;
 	}
 
-	public void setSaldo(BigDecimal saldo) {
+	protected void setSaldo(BigDecimal saldo) {
 		this.saldo = saldo;
 	}
 
 	public LocalDateTime getDataAbertura() {
 		return dataAbertura;
-	}
-
-	public void setDataAbertura(LocalDateTime dataAbertura) {
-		this.dataAbertura = dataAbertura;
 	}
 
 	public boolean isStatus() {
@@ -58,37 +49,39 @@ public abstract class Conta {
 		return transacoes;
 	}
 
-	public void setTransacoes(List<Transacao> transacoes) {
-		this.transacoes = transacoes;
-	}
-
+	@Override
 	public void realizarDeposito(BigDecimal quantia) {
-		if (quantia.compareTo(BigDecimal.ZERO) <= 0) {
-			throw new IllegalArgumentException("Quantia inválida para depósito.");
+		if (quantia.compareTo(BigDecimal.ZERO) > 0) {
+			saldo = saldo.add(quantia);
+			transacoes.add(new Transacao(quantia, LocalDateTime.now(), "CREDITO", null));
+			System.out.println("Depósito realizado com sucesso.");
+		} else {
+			System.out.println("Valor inválido para depósito.");
 		}
-		this.saldo = this.saldo.add(quantia);
-		transacoes.add(new Transacao(quantia, LocalDateTime.now(), "CREDITO", null));
-	}
-
-	public abstract boolean realizarSaque(BigDecimal quantia);
-
-	public abstract boolean realizarTransferencia(Conta destino, BigDecimal quantia);
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(numero);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Conta other = (Conta) obj;
-		return Objects.equals(numero, other.numero);
+	public void realizarSaque(BigDecimal quantia) {
+		if (quantia.compareTo(BigDecimal.ZERO) > 0 && saldo.compareTo(quantia) >= 0) {
+			saldo = saldo.subtract(quantia);
+			transacoes.add(new Transacao(quantia, LocalDateTime.now(), "DEBITO", null));
+			System.out.println("Saque realizado com sucesso.");
+		} else {
+			System.out.println("Saldo insuficiente ou valor inválido para saque.");
+		}
+	}
+
+	@Override
+	public void realizarTransferencia(IConta destino, BigDecimal quantia) {
+		if (quantia.compareTo(BigDecimal.ZERO) > 0 && saldo.compareTo(quantia) >= 0) {
+			saldo = saldo.subtract(quantia);
+			destino.setSaldo(destino.getSaldo().add(quantia));
+			transacoes.add(new Transacao(quantia, LocalDateTime.now(), "TRANSFERÊNCIA_DEBITO", destino));
+			destino.getTransacoes().add(new Transacao(quantia, LocalDateTime.now(), "TRANSFERÊNCIA_CREDITO", this));
+			System.out.println("Transferência realizada com sucesso.");
+		} else {
+			System.out.println("Saldo insuficiente ou valor inválido para transferência.");
+		}
 	}
 
 	@Override
@@ -96,5 +89,4 @@ public abstract class Conta {
 		return "Conta [numero=" + numero + ", saldo=" + saldo + ", dataAbertura=" + dataAbertura + ", status=" + status
 				+ "]";
 	}
-
 }
